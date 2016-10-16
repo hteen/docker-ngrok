36786214ee6209b12fcd83b476ff6b27f772d5f3
hteen <i@hteen.cn>
2016-10-16 13:28:59 +0800

    fix bug

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ if [ "${DOMAIN}" == "**None**" ]; then
 fi
 
 cd ${MY_FILES}
-
-openssl genrsa -out base.key 2048
-openssl req -new -x509 -nodes -key base.key -days 10000 -subj "/CN=${DOMAIN}" -out base.pem
-openssl genrsa -out device.key 2048
-openssl req -new -key device.key -subj "/CN=${DOMAIN}" -out device.csr
-openssl x509 -req -in device.csr -CA base.pem -CAkey base.key -CAcreateserial -days 10000 -out device.crt
-
+if [ ! -f "${MY_FILES}/base.pem" ]; then
+    openssl genrsa -out base.key 2048
+    openssl req -new -x509 -nodes -key base.key -days 10000 -subj "/CN=${DOMAIN}" -out base.pem
+    openssl genrsa -out device.key 2048
+    openssl req -new -key device.key -subj "/CN=${DOMAIN}" -out device.csr
+    openssl x509 -req -in device.csr -CA base.pem -CAkey base.key -CAcreateserial -days 10000 -out device.crt
+fi
 cp -r base.pem /ngrok/assets/client/tls/ngrokroot.crt
 
 cd /ngrok
