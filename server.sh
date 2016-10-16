13272bfc5d37732d3b85ed1aea3424edd629ec27
hteen <i@hteen.cn>
2016-10-16 11:44:40 +0800

    edit

--- server.sh
+++ server.sh
@@ -6,4 +6,10 @@ if [ "${DOMAIN}" == "**None**" ]; then
     exit 1
 fi
 
+if [ ! -f "${MY_FILES}/bin/ngrokd" ]; then
+    echo "ngrokd is not build,will be build it now..."
+    /bin/bash ./build.sh
+fi
+
+
 ${MY_FILES}/bin/ngrokd -tlsKey=${MY_FILES}/device.key -tlsCrt=${MY_FILES}/device.crt -domain="${DOMAIN}" -httpAddr=${HTTP_ADDR} -httpsAddr=${HTTPS_ADDR} -tunnelAddr=${TUNNEL_ADDR}
