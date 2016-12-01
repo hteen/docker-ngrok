1d6692313a44f8fb5d4511cca9c3e79ebfd35d6b
hteen <i@hteen.cn>
2016-12-02 00:08:33 +0800

    编译客户端
    Linux 平台 32 位系统：GOOS=linux GOARCH=386
    Linux 平台 64 位系统：GOOS=linux GOARCH=amd64
    
    Windows 平台 32 位系统：GOOS=windows GOARCH=386
    Windows 平台 64 位系统：GOOS=windows GOARCH=amd64
    
    MAC 平台 32 位系统：GOOS=darwin GOARCH=386
    MAC 平台 64 位系统：GOOS=darwin GOARCH=amd64
    
    ARM 平台：GOOS=linux GOARCH=arm

--- build.sh
+++ build.sh
@@ -18,9 +18,13 @@ cp -r base.pem /ngrok/assets/client/tls/ngrokroot.crt
 
 cd /ngrok
 make release-server
-make release-client
+GOOS=linux GOARCH=386 make release-client
+GOOS=linux GOARCH=amd64 make release-client
+GOOS=windows GOARCH=386 make release-client
 GOOS=windows GOARCH=amd64 make release-client
+GOOS=darwin GOARCH=386 make release-client
 GOOS=darwin GOARCH=amd64 make release-client
+GOOS=linux GOARCH=arm make release-client
 
 cp -r /ngrok/bin ${MY_FILES}/bin
 
